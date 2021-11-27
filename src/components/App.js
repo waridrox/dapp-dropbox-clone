7f9caf1bea64832143764e9d3c401b70682d1ebc
waridrox <mohdwarid4@gmail.com>
2021-11-27 15:06:55 +0530

    gettimg smart contract data

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-//import DStorage from '../abis/DStorage.json'
+import DStorage from '../abis/DStorage.json'
 import React, { Component } from 'react';
 import Navbar from './Navbar'
 import Main from './Main'
@@ -6,6 +6,8 @@ import Web3 from 'web3';
 import './App.css';
 
 //Declare IPFS
+const ipfsClient = require('ipfs-http-client')
+const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https'}) //leaving out the protocol will default to these values
 
 class App extends Component {
 
@@ -39,17 +41,33 @@ class App extends Component {
     this.setState({account: accounts[0]})
 
     //Network ID
+    const networkId = await web3.eth.net.getId()
+    const networkData = DStorage.networks[networkId]
 
     //IF got connection, get data from contracts
+    if (networkData) {
       //Assign contract
+      const dstorage = new web3.eth.Contract(DStorage.abi, networkData.address)
+      this.setState( {dstorage} )
 
       //Get files amount
+      const filesCount = await dstorage.methods.fileCount().call()
+      this.setState( {filesCount} )
 
       //Load files&sort by the newest
-
+      for (var i = filesCount; i >= 1; i--) {
+        const file = await dstorage.methods.files(i).call()
+        this.setState({
+          files: [...this.state.files, file]
+        })
+      }
+    }
     //Else
-      //alert Error
-
+    //alert Error
+    else {
+      window.alert('DStorage contract not yet deployed to attach to the network.')
+    }
+    this.setState({loading: false})
   }
 
   // Get file from user
@@ -77,7 +95,12 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      loading: false
+      account: '',
+      dstorage: null,
+      files: [],
+      loading: false,
+      type: null,
+      name: null
     }
 
     //Bind functions
